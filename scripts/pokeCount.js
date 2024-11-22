3f55384dd4dc9c8e4612e1ce7fd5a172f8bb8950
Finn Grünhage <finn@hellgruen.net>
2024-11-22 20:11:01 +0100

    Hotfix error message

--- scripts/pokeCount.js
+++ scripts/pokeCount.js
@@ -1,11 +1,11 @@
 function setPokeCount(){
     clearMain();
     latestPokemon = 1;
-    pokemonCount = document.getElementById("numberInput").value;
+    pokemonCount = Number(document.getElementById("numberInput").value);
     if (pokemonCount == "") {
         pokemonCount = 20;
     }
-    if (pokemonCount <= 1000 || pokemonCount >= 1) {
+    if (pokemonCount <= 1000 && pokemonCount >= 1) {
         if (search.length >= 3) {
             loadAndShowSelectedPokemon()
         } else if (search.length == 0) {
